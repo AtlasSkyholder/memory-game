beef1d02981aa89f2bcfcbd1309112f4763566b8
morbi <the_morbidus@hotmail.com>
2020-07-27 21:00:26 -0400

    game functionality part 1

--- app.js
+++ app.js
@@ -4,6 +4,64 @@ document.addEventListener('DOMContentLoaded', () => {
   const cardArray = [
     {
       name: 'fries',
+      img: 'images/fries.jpg'
+    },
+    {
+      name: 'fries',
+      img: 'images/fries.jpg'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.jpg'
+    },
+    {
+      name: 'cheeseburger',
+      img: 'images/cheeseburger.jpg'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.jpg'
+    },
+    {
+      name: 'hotdog',
+      img: 'images/hotdog.jpg'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.jpg'
+    },
+    {
+      name: 'ice-cream',
+      img: 'images/ice-cream.jpg'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.jpg'
+    },
+    {
+      name: 'milkshake',
+      img: 'images/milkshake.jpg'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.jpg'
+    },
+    {
+      name: 'pizza',
+      img: 'images/pizza.jpg'
     }
   ]
+
+  const grid = document.querySelector('.grid');
+
+  //create your board
+  function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
+      let card = document.createElement('img');
+      card.setAttribute('src', 'images/blank.jpg');
+      card.setAttribute('data-id', i);
+      // card.addEventListener('click', flipcard)
+      grid.appendChild(card)
+    }
+  }
 })
